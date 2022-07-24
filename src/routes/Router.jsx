70d7608a9f0f05d4477c619d25e17e542b2a4e1b
Sakiyama Yuki <yuki.king.0422@gmail.com>
2022-07-24 22:22:08 +0900

    update
    ルーティング内のAuthentificationを修正

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -10,29 +10,77 @@ import { EditTask } from "../pages/EditTask";
 import { SignUp } from "../pages/SignUp";
 import { EditList } from "../pages/EditList";
 
-export const Router = () => {
+const AuthRouter = () => {
   const auth = useSelector((state) => state.auth.isSignIn);
+  if (!auth) {
+    return <Navigate to="/signin" />;
+  }
+};
+
+export const Router = () => {
+  // const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-        {auth ? (
-          <>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/task/new" component={NewTask} />
-            <Route exact path="/list/new" component={NewList} />
-            <Route
-              exact
-              path="/lists/:listId/tasks/:taskId"
-              component={EditTask}
-            />
-            <Route exact path="/lists/:listId/edit" component={EditList} />
-          </>
+        <Route exact path="/signin" component={SignIn} element={<SignIn />} />
+        <Route exact path="/signup" component={SignUp} element={<SignUp />} />
+        {/* {auth ? (
+          <> */}
+        <Route
+          exact
+          path="/"
+          component={Home}
+          element={
+            <AuthRouter>
+              <Home />
+            </AuthRouter>
+          }
+        />
+        <Route
+          exact
+          path="/task/new"
+          component={NewTask}
+          element={
+            <AuthRouter>
+              <NewTask />
+            </AuthRouter>
+          }
+        />
+        <Route
+          exact
+          path="/list/new"
+          component={NewList}
+          element={
+            <AuthRouter>
+              <NewList />
+            </AuthRouter>
+          }
+        />
+        <Route
+          exact
+          path="/lists/:listId/tasks/:taskId"
+          component={EditTask}
+          element={
+            <AuthRouter>
+              <EditTask />
+            </AuthRouter>
+          }
+        />
+        <Route
+          exact
+          path="/lists/:listId/edit"
+          component={EditList}
+          element={
+            <AuthRouter>
+              <EditList />
+            </AuthRouter>
+          }
+        />
+        {/* </>
         ) : (
           <Route component={<Navigate to="/signin" />} />
-        )}
+        )} */}
         <Route component={NotFound} />
       </Routes>
     </BrowserRouter>
